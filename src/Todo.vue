81f987c6ee04c83da1b6a177fee96b5a592e437d
DannyHTML <dannyjag@gmail.com>
2023-07-21 16:40:26 +0200

    some small progress

--- src/Todo.vue
+++ src/Todo.vue
@@ -5,7 +5,9 @@
         <h1 class="uppercase text-2xl font-bold mb-4">t o d o</h1>
       </div>
       <div class="relative mx-8 flex items-center mb-4">
-        <div class="absolute ml-4 border-2 h-6 w-6 rounded-full"></div>
+        <div
+          class="absolute ml-4 border-2 h-6 w-6 rounded-full border-white border-opacity-30"
+        ></div>
         <input
           class="p-2 w-full rounded-md bg-gray-800 pl-14 text-white outline-0"
           v-model="newTodo"
@@ -14,30 +16,34 @@
           name="todo"
           id="todo"
           placeholder="Create a new todo..."
+          maxlength="30"
         />
       </div>
-      <ul
-        class="text-white mx-8 capitalize"
-        :class="{
-          active: active,
-          'text-opacity-30 line-through': !active,
-        }"
-      >
+      <ul class="text-white mx-8 capitalize">
         <li
           v-for="(todo, index) in todos"
           :key="index"
-          class="border-b w-full bg-gray-800 border-gray-500"
+          class="border-b w-full bg-gray-800 border-gray-500 text-white"
+          :class="{
+            active: active,
+            'text-opacity-30 line-through': active[index],
+          }"
         >
           <div class="relative mx-4 py-4 pb-3 flex items-center">
             <div
-              class="absolute border-2 h-6 w-6 rounded-full"
+              class="absolute border-2 h-6 w-6 rounded-full border-white border-opacity-30 cursor-pointer"
+              :class="{
+                active: active[index],
+                'bg-gradient-to-br from-blue-500 to-purple-700 border-opacity-0':
+                  active[index],
+              }"
               @click="completed(index)"
             >
               <img
-                class="absolute translate-x-1/2 translate-y-1/2"
+                class="absolute select-none translate-x-1/2 translate-y-1/2"
                 src="/images/icon-check.svg"
                 alt=""
-                :class="{ hidden: isHidden }"
+                :class="{ hidden: !active[index] }"
               />
             </div>
             <span class="ml-10">{{ todo }}</span>
@@ -55,25 +61,27 @@
 import { ref } from "vue";
 
 const todos = ref([]);
-const newTodo = ref("");
-const isHidden = ref([]);
+let newTodo = ref("");
 const active = ref([]);
+let items = ref(0);
 
 const addTodo = () => {
   if (newTodo.value.trim() !== "") {
     todos.value.push(newTodo.value);
     newTodo.value = "";
+    items.value++;
   }
 };
 
 const removeTodo = (index) => {
   todos.value.splice(index, 1);
+  active.value.splice(index, 1);
+  items.value--;
+  console.log(items.value);
 };
 
 const completed = (index) => {
-  isHidden.value = !isHidden.value;
-  active.value = !active.value;
-  console.log("object");
+  active.value[index] = !active.value[index];
 };
 </script>
 
